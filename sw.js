beb14e9dcc1a3a17d683b342dec1b0736666be8a
thiepn <junsosmart@gmail.com>
2026-09-22 22:06:18 +0200

    pwa: roll cache for completed Statistics V2 phase

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const CACHE="calc-shell-v9";
+const CACHE="calc-shell-v10";
 const ASSETS=["./","./index.html","./styles.css","./math.js","./algebra.js","./calculus.js","./calculus-worker.js","./units.js","./linear-algebra.js","./statistics.js","./statistics-worker.js","./app.js","./manifest.webmanifest","./icon.svg"];
 const shellUrl=new URL("./index.html",self.registration.scope).href;
 
