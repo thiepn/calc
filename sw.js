e2d7d0d729badd5bc1f4c3ac86de2bc6a41c97a2
thiepn <junsosmart@gmail.com>
2026-09-23 16:26:46 +0200

    release: cache production release metadata offline

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 "use strict";
 const APP_VERSION="1.0.0";
 const CACHE="calc-shell-v"+APP_VERSION;
-const ASSETS=["./","./index.html","./styles.css","./math.js","./algebra.js","./calculus.js","./calculus-worker.js","./units.js","./linear-algebra.js","./statistics.js","./statistics-worker.js","./graph.js","./graph-worker.js","./tools.js","./custom-tools.js","./notebook.js","./persistence.js","./app.js","./manifest.webmanifest","./icon.svg"];
+const ASSETS=["./","./index.html","./styles.css","./math.js","./algebra.js","./calculus.js","./calculus-worker.js","./units.js","./linear-algebra.js","./statistics.js","./statistics-worker.js","./graph.js","./graph-worker.js","./tools.js","./custom-tools.js","./notebook.js","./persistence.js","./app.js","./manifest.webmanifest","./release.json","./icon.svg"];
 const shellUrl=new URL("./index.html",self.registration.scope).href;
 
 self.addEventListener("install",function(event){
